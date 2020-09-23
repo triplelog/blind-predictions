116dfabf09f9121e6c1dd918bfa4e4afc564a7d7
triplelog <robmwilc@gmail.com>
2020-09-23 16:23:34 -0400

    nm

---
+++ createData.py
@@ -0,0 +1,95 @@
+initial = ['std::map<int,std::vector<double> > createCorrelations(){\nstd::map<int,std::vector<double> > correlations;\n']
+end = ['return correlations;\n}\n']
+file1 = open('data.cpp', 'w')
+file1.writelines(initial) 
+ 
+print("hello")
+# Using readlines() 
+file2 = open('correlations.csv', 'r') 
+lines = file2.readlines() 
+  
+count = 0
+# Strips the newline character 
+i = 0
+for line in lines:
+	i+=1
+	if i==1:
+		continue
+	lineSplit = line.strip().split(",")
+	lineText = "{"
+	for ii in range(1,52):
+		lineText += " "+lineSplit[ii]
+		if ii < 51:
+			lineText += ","
+		else:
+			lineText += " }"
+	line1 = 'double arr'+lineSplit[0]+'[] = '+lineText+';\n';
+	line2 = 'std::vector<double> vect'+lineSplit[0]+';\n';
+	line3 = "";
+	for ii in range(0,51):
+		line3 += 'vect'+lineSplit[0]+'.push_back(arr'+lineSplit[0]+'['+str(ii)+']); ';
+	line4 = '\ncorrelations['+str(i-2)+']= vect'+lineSplit[0]+';\n'
+	file1.writelines([line1,line2,line3,line4])
+file1.writelines(end)
+
+initial = ['std::vector<std::string> createStates(){\nstd::vector<std::string> states;\n']
+end = ['return states;\n}\n']
+file1.writelines(initial) 
+i = 0
+for line in lines:
+	i+=1
+	if i==1:
+		continue
+	lineSplit = line.strip().split(",")
+	lineText = "{"
+	for ii in range(1,52):
+		lineText += lineSplit[ii]
+		if ii < 51:
+			lineText += ","
+		else:
+			lineText += "}"
+	line2 = 'states.push_back("'+lineSplit[0]+'");\n'
+	file1.writelines([line2])
+file1.writelines(end)
+
+
+initial = ['std::vector<double> createPredictions(){\nstd::vector<double> predictions;\n']
+end = ['return predictions;\n}\n']
+file2 = open('predictions.csv', 'r') 
+lines = file2.readlines() 
+
+file1.writelines(initial) 
+i = 0
+for line in lines:
+	i+=1
+	if i==1:
+		continue
+	lineSplit = line.strip().split(",")
+	pred = float(lineSplit[7])
+	if pred > .99:
+		pred = .99
+	elif pred < .01:
+		pred = .01
+	#print(lineSplit[3],pred)
+	line2 = 'predictions.push_back('+str(pred)+');\n'
+	file1.writelines([line2])
+file1.writelines(end)
+
+initial = ['std::vector<int> createEV(){\nstd::vector<int> evs;\n']
+end = ['return evs;\n}\n']
+file2 = open('ev.csv', 'r') 
+lines = file2.readlines() 
+
+file1.writelines(initial) 
+i = 0
+for line in lines:
+	i+=1
+	if i==1:
+		continue
+	lineSplit = line.strip().split(",")
+	ev = int(lineSplit[8])
+
+	line2 = 'evs.push_back('+str(ev)+');\n'
+	file1.writelines([line2])
+file1.writelines(end)
+file1.close()
---
+++ main.cpp
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <math.h>
+#include <limits.h>
+#include <time.h>
+#include <string.h> 
+#include <ctype.h>
+#include <cmath>
+#include <variant>
+#include <map>
+#include <numeric>
+#include <chrono>
+#include <thread>
+#include <array>
+#include <vector>
+#include <unistd.h>
+#include <thread>
+#include <future>
+
+#include "data.cpp"
+
+#include <emscripten/emscripten.h>
+
+EM_JS(void, console_log, (int x), {
+  console.log(x);
+});
+
+std::map<int,std::vector<double> > correlations;
+std::vector<std::string> states;
+std::vector<int> evs;
+std::vector<double> predictions;
+	
+int predictionToElo(double prediction){
+	double e = log(1/prediction-1)*-400/log(10.0);
+	return 1500+round(e);
+	
+}
+double predictionFromElo(int elo){
+	double e = 1500-elo;
+	return 1/(1+pow(10,e/400));
+	
+}
+
+extern "C" {
+
+void makePrediction() {
+	std::vector<int> elo;
+	int i; int ii; int iii;
+	for (i = 0;i<predictions.size();i++){
+		//std::cout << predictionToElo(predictions[i]) << "\n";
+		elo.push_back(predictionToElo(predictions[i]));
+	}
+	//predict one state at a time
+	int bidenWins = 0;
+	std::map<int,int> evData;
+	for (i=0;i<1000;i++){
+		std::vector<int> elonew = elo;
+		int bidenEV = 0;
+		std::map<int,bool> doneYet;
+		for (ii=0;ii<51;ii++){
+			int thisstate = rand() % 51;
+			while (doneYet.find(thisstate) != doneYet.end()){
+				thisstate = rand() % 51;
+			}
+			doneYet[thisstate]=true;
+			double pred = predictionFromElo(elonew[thisstate]);
+			//std::cout << "Biden prob for "+states[ii] + " is " << pred << " not " << predictionFromElo(elo[ii]) << "\n";
+			int r = rand() % 1000;
+			double rr = r;
+			rr /= 1000;
+			int elodiff = 0;
+			int eloNum = 1;
+			int eloDen = 5;
+			if (rr < pred){ // Biden wins
+				elodiff = 2300 - elonew[thisstate];
+				elodiff *= eloNum;
+				elodiff /= eloDen;
+				//std::cout << "Biden wins "+states[ii] << "\n";
+				bidenEV += evs[thisstate];
+			}
+			else {
+				elodiff = 700 - elonew[thisstate];
+				elodiff *= eloNum;
+				elodiff /= eloDen;
+				//std::cout << "Biden loses "+states[ii] << "\n";
+			}
+			for (iii=0;iii<51;iii++){
+				elonew[iii]=elonew[iii]+elodiff*correlations[thisstate][iii];
+			}
+		}
+		if (bidenEV >= 270){
+			bidenWins++;
+		}
+		if (evData.find(bidenEV) == evData.end()){
+			evData[bidenEV] = 1;
+		}
+		else {
+			evData[bidenEV]++;
+		}
+	}
+	console_log(bidenWins);
+	for (i=0;i<539;i++){
+		if (evData.find(i) != evData.end()){
+			//std::cout << i << ", " << evData[i] << "\n";
+		}
+	}
+}
+
+}
+
+void initialRun(){
+	correlations = createCorrelations();
+	states = createStates();
+	evs = createEV();
+	predictions = createPredictions();
+	
+
+}
+
+int main() {
+	srand(time(NULL));
+	initialRun();
+	makePrediction();
+	return 1;
+}
