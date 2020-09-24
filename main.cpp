3fea4e97bf5a7d84dc1bae67cf6fc3c235d86b75
triplelog <robmwilc@gmail.com>
2020-09-24 11:41:32 -0400

    nm

--- main.cpp
+++ main.cpp
@@ -138,7 +138,7 @@ void makePrediction(int year) {
 			}
 			
 			for (iii=0;iii<51;iii++){
-				int elodiff = round(eloR*pow(10,correlations[thisstate][iii])/100);
+				int elodiff = round(eloR*pow(correlations[thisstate][iii],10)*100);
 				elonew[iii]=elonew[iii]+elodiff;
 			}
 		}
