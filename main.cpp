cce8562f7a066c115afb071b40b44dc795013c80
triplelog <robmwilc@gmail.com>
2020-09-24 10:34:49 -0400

    nm

--- main.cpp
+++ main.cpp
@@ -109,7 +109,7 @@ void makePrediction(int year) {
 			//convert rr to vote percentage
 			int eloR = predictionToElo(rr);
 			
-			int elodiff = eloR / 17;
+			int elodiff = eloR / 15;
 			if (elonew[thisstate]+eloR > 0){ // Biden wins
 				
 				bidenEV += evs[thisstate];
