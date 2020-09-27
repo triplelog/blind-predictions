21c52fa516038c86df550d1805f259be1c0479cc
triplelog <robmwilc@gmail.com>
2020-09-27 09:11:59 -0400

    nm

--- static/js/housemap.js
+++ static/js/housemap.js
@@ -54,9 +54,9 @@ for (var i=0;i<200;i++){
 		}
 	}
 }
-console.log(natadv+adj218);
 
-
+var demProb218 = 1.0/(1.0+Math.pow(10.0,-10*adj218/75));
+console.log(natadv+adj218, demProb218);
 
 demVote = 0;
 repVote = 0;
