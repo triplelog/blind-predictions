f9327434389a15105088b3bd5e704057feda1aa2
triplelog <robmwilc@gmail.com>
2020-09-28 12:45:42 -0400

    nm

--- static/js/senatemap.js
+++ static/js/senatemap.js
@@ -273,7 +273,7 @@ myWorker.onmessage = function(e) {
 		}
 		for (var i=6;i<16;i++){	
 			data.labels.push(""+(minSeats+i));
-			data.series[0].push(demTotal[i]);
+			data.series[0].push(1000 - demTotal[i-1]);
 			data.series[1].push(demTotal[i]);
 		}
 		console.log(data);
@@ -287,7 +287,7 @@ myWorker.onmessage = function(e) {
 		};
 
 		var responsiveOptions = [
-		  ['screen and (max-width: 1640px)', {
+		  ['screen and (max-width: 640px)', {
 			seriesBarDistance: 5,
 			axisX: {
 			  labelInterpolationFnc: function (value) {
