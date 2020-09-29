65511902826090cee43827b8c32de081631a20c8
triplelog <robmwilc@gmail.com>
2020-09-29 09:04:34 -0400

    nm

--- seam-carving/carving.cpp
+++ seam-carving/carving.cpp
@@ -305,7 +305,7 @@ void initialRun(){
 	auto a11 = std::chrono::high_resolution_clock::now();
 	for (i=0;i<100;i++){
 		if (!killCarve){
-			m = verticalSeam(m,2);
+			m = verticalSeam(m,1);
 		}
 		if (!killCarve){
 			m = horizontalSeam(m);
