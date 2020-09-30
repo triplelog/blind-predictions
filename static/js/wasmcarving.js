a985289fea3b818d7012381b949d63273fcb8b44
triplelog <robmwilc@gmail.com>
2020-09-30 11:25:59 -0400

    nm

--- static/js/wasmcarving.js
+++ static/js/wasmcarving.js
@@ -6,7 +6,7 @@ function cpp_ready() {
 
 function displayNow() {
 	var svg = '<svg style="width: 60vw;" viewBox="-1 -1 '+(parseInt(maxX)+2)+' '+(parseInt(maxY)+2)+'" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  version="1.2" baseProfile="tiny">';
-	var maxP = 0; var minP = 1000000000;
+	var maxP = 0; var minP = 1000000000; var sumP = 0;
 	for (var i=0;i<jsPoints.length;i++){
 		if (parseInt(jsPoints[i].val)>maxP){
 			maxP = parseInt(jsPoints[i].val);
@@ -14,10 +14,12 @@ function displayNow() {
 		if (parseInt(jsPoints[i].val)<minP){
 			minP = parseInt(jsPoints[i].val);
 		}
+		sumP += parseInt(jsPoints[i].val);
 	}
-	console.log(minP,maxP);
+	var avgP = sumP/jsPoints.length;
+	console.log(minP,maxP, avgP);
 	for (var i=0;i<jsPoints.length;i++){
-		svg += '<circle r=".5" cx="'+jsPoints[i].x+'" cy="'+jsPoints[i].y+'" fill="rgb('+(parseInt(jsPoints[i].val)*250/maxP)+',0,'+(250-parseInt(jsPoints[i].val)*250/maxP)+')" stroke="none"></circle>';
+		svg += '<circle r=".5" cx="'+jsPoints[i].x+'" cy="'+jsPoints[i].y+'" fill="rgb('+(parseInt(jsPoints[i].val)*125/avgP)+',0,'+(250-parseInt(jsPoints[i].val)*125/avgP)+')" stroke="none"></circle>';
 	}
 	svg += '</svg>';
 	postMessage(svg);
