9340188d5b936e945e4dfb4d7cfaa6d936e8cba7
triplelog <robmwilc@gmail.com>
2020-10-01 11:00:35 -0400

    nm

--- seam-carving/carving.cpp
+++ seam-carving/carving.cpp
@@ -64,6 +64,7 @@ bool killCarveH;
 int vertThreads;
 int horzThreads;
 int np;
+std::string datas[4];
 
 extern "C" {
 
@@ -1461,11 +1462,7 @@ Map horizontalStitch(Map m, int n, int l){
 				p0.val = v - v/4 - v/4 - v/4;
 				p1.val = v/4;
 				p2.val = v/4;
-				std::string datas[4];
-				datas[0] = "d";
-				datas[1] = "r";
-				datas[2] = "d16";
-				datas[3] = "r16";
+				
 				for (ii=0;ii<4;ii++){
 					std::string s = datas[ii];
 					int r = p0.data[s]+pn1.data[s]+p1.data[s];
@@ -1488,95 +1485,80 @@ Map horizontalStitch(Map m, int n, int l){
 				for(ii=m.height;ii>removeSeam[i]+2;ii--){
 					m.pixels[i][ii]=m.pixels[i][ii-1];
 				}
-				m.pixels[i][removeSeam[i]+1].county =m.pixels[i][removeSeam[i]].county;
-				int v = m.pixels[i][removeSeam[i]].val+m.pixels[i][removeSeam[i]+1].val;
-				int r = m.pixels[i][removeSeam[i]].data["r"]+m.pixels[i][removeSeam[i]+1].data["r"];
-				int d = m.pixels[i][removeSeam[i]].data["d"]+m.pixels[i][removeSeam[i]+1].data["d"];
-				int r16 = m.pixels[i][removeSeam[i]].data["r16"]+m.pixels[i][removeSeam[i]+1].data["r16"];
-				int d16 = m.pixels[i][removeSeam[i]].data["d16"]+m.pixels[i][removeSeam[i]+1].data["d16"];
+				Point p0 = m.pixels[i][removeSeam[i]];
+				Point p1 = m.pixels[i][removeSeam[i]+1];
+				Point p2 = m.pixels[i][removeSeam[i]+2];
+				p1.county =p0.county;
+				int v = p0.val+p1.val;
 				int xx; int yy;
 				if (v <= 0){
-					xx = m.pixels[i][removeSeam[i]].x;
-					yy = m.pixels[i][removeSeam[i]].y;
+					xx = p0.x;
+					yy = p0.y;
 				}
 				else {
-					xx = m.pixels[i][removeSeam[i]].val*m.pixels[i][removeSeam[i]].x;
-					xx += m.pixels[i][removeSeam[i]+1].val*m.pixels[i][removeSeam[i]+1].x;
+					xx = p0.val*p0.x;
+					xx += p1.val*p1.x;
 					xx += v/2;
 					xx = xx/v;
-					yy = m.pixels[i][removeSeam[i]].val*m.pixels[i][removeSeam[i]].y;
-					yy += m.pixels[i][removeSeam[i]+1].val*m.pixels[i][removeSeam[i]+1].y;
+					yy = p0.val*p0.y;
+					yy += p1.val*p1.y;
 					yy += v/2;
 					yy = yy/v;
 				}
 				
-				m.pixels[i][removeSeam[i]].val = v - v/3 - v/3;
-				m.pixels[i][removeSeam[i]+1].val = v/3;
-				m.pixels[i][removeSeam[i]+2].val = v/3;
+				p0.val = v - v/3 - v/3;
+				p1.val = v/3;
+				p2.val = v/3;
 				
-				m.pixels[i][removeSeam[i]].data["r"] = r - r/3 - r/3;
-				m.pixels[i][removeSeam[i]+1].data["r"] = r/3;
-				m.pixels[i][removeSeam[i]+2].data["r"] = r/3;
+				for (ii=0;ii<4;ii++){
+					std::string s = datas[ii];
+					int r = p0.data[s]+p1.data[s];
+					p0.data[s] = r - r/3 - r/3;
+					p1.data[s] = r/3;
+					p2.data[s] = r/3;
+				}
 				
-				m.pixels[i][removeSeam[i]].data["d"] = d - d/3 - d/3;
-				m.pixels[i][removeSeam[i]+1].data["d"] = d/3;
-				m.pixels[i][removeSeam[i]+2].data["d"] = d/3;
+				p0.x = xx;
+				p1.x = xx;
+				p2.x = xx;
 				
-				m.pixels[i][removeSeam[i]].data["r16"] = r16 - r16/3 - r16/3;
-				m.pixels[i][removeSeam[i]+1].data["r16"] = r16/3;
-				m.pixels[i][removeSeam[i]+2].data["r16"] = r16/3;
-				
-				m.pixels[i][removeSeam[i]].data["d16"] = d16 - d16/3 - d16/3;
-				m.pixels[i][removeSeam[i]+1].data["d16"] = d16/3;
-				m.pixels[i][removeSeam[i]+2].data["d16"] = d16/3;
-				
-				m.pixels[i][removeSeam[i]].x = xx;
-				m.pixels[i][removeSeam[i]+1].x = xx;
-				m.pixels[i][removeSeam[i]+2].x = xx;
-				
-				m.pixels[i][removeSeam[i]].y = yy;
-				m.pixels[i][removeSeam[i]+1].y = yy;
-				m.pixels[i][removeSeam[i]+2].y = yy;
+				p0.y = yy;
+				p1.y = yy;
+				p2.y = yy;
 			}
 			else {
 				for(ii=m.height;ii>removeSeam[i]+1;ii--){
 					m.pixels[i][ii]=m.pixels[i][ii-1];
 				}
-				m.pixels[i][removeSeam[i]+1].county =m.pixels[i][removeSeam[i]].county;
-				int v = m.pixels[i][removeSeam[i]].val;
-				int r = m.pixels[i][removeSeam[i]].data["r"];
-				int d = m.pixels[i][removeSeam[i]].data["d"];
-				int r16 = m.pixels[i][removeSeam[i]].data["r16"];
-				int d16 = m.pixels[i][removeSeam[i]].data["d16"];
+				Point p0 = m.pixels[i][removeSeam[i]];
+				Point p1 = p1;
+				p1.county =p0.county;
+				int v = p0.val;
 				int xx; int yy;
 				if (v <= 0){
-					xx = m.pixels[i][removeSeam[i]].x;
-					yy = m.pixels[i][removeSeam[i]].y;
+					xx = p0.x;
+					yy = p0.y;
 				}
 				else {
-					xx = m.pixels[i][removeSeam[i]].x;
-					yy = m.pixels[i][removeSeam[i]].y;
+					xx = p0.x;
+					yy = p0.y;
 				}
-				m.pixels[i][removeSeam[i]].val = v/2;
-				m.pixels[i][removeSeam[i]+1].val = v - v/2;
+				p0.val = v/2;
+				p1.val = v - v/2;
 				
-				m.pixels[i][removeSeam[i]].data["r"] = r/2;
-				m.pixels[i][removeSeam[i]+1].data["r"] = r - r/2;
+				for (ii=0;ii<4;ii++){
+					std::string s = datas[ii];
+					int r = p0.data[s];
+					p0.data[s] = r/2;
+					p1.data[s] = r - r/2;
+				}
 				
-				m.pixels[i][removeSeam[i]].data["d"] = d/2;
-				m.pixels[i][removeSeam[i]+1].data["d"] = d - d/2;
 				
-				m.pixels[i][removeSeam[i]].data["r16"] = r16/2;
-				m.pixels[i][removeSeam[i]+1].data["r16"] = r16 - r16/2;
+				p0.x = xx;
+				p1.x = xx;
 				
-				m.pixels[i][removeSeam[i]].data["d16"] = d16/2;
-				m.pixels[i][removeSeam[i]+1].data["d16"] = d16 - d16/2;
-				
-				m.pixels[i][removeSeam[i]].x = xx;
-				m.pixels[i][removeSeam[i]+1].x = xx;
-				
-				m.pixels[i][removeSeam[i]].y = yy;
-				m.pixels[i][removeSeam[i]+1].y = yy;
+				p0.y = yy;
+				p1.y = yy;
 			}
 		}
 		m.height++;
@@ -1655,6 +1637,10 @@ Map verticalStitch(Map m, int n, int l){
 			
 					m.pixels[i][ii]=m.pixels[i-1][ii];
 				}
+				Point pn1 = m.pixels[removeSeam[ii]-1][ii];
+				Point p0 = m.pixels[removeSeam[ii]][ii];
+				Point p1 = m.pixels[removeSeam[ii]+1][ii];
+				Point p2 = m.pixels[removeSeam[ii]+2][ii];
 				m.pixels[removeSeam[ii]+1][ii].county=m.pixels[removeSeam[ii]][ii].county;
 				int v = m.pixels[removeSeam[ii]][ii].val+m.pixels[removeSeam[ii]-1][ii].val+m.pixels[removeSeam[ii]+1][ii].val;
 				int r = m.pixels[removeSeam[ii]][ii].data["r"]+m.pixels[removeSeam[ii]-1][ii].data["r"]+m.pixels[removeSeam[ii]+1][ii].data["r"];
@@ -1718,6 +1704,9 @@ Map verticalStitch(Map m, int n, int l){
 			
 					m.pixels[i][ii]=m.pixels[i-1][ii];
 				}
+				Point p0 = m.pixels[removeSeam[ii]][ii];
+				Point p1 = m.pixels[removeSeam[ii]+1][ii];
+				Point p2 = m.pixels[removeSeam[ii]+2][ii];
 				m.pixels[removeSeam[ii]+1][ii].county=m.pixels[removeSeam[ii]][ii].county;
 				int v = m.pixels[removeSeam[ii]][ii].val+m.pixels[removeSeam[ii]+1][ii].val;
 				int r = m.pixels[removeSeam[ii]][ii].data["r"]+m.pixels[removeSeam[ii]+1][ii].data["r"];
@@ -1772,6 +1761,8 @@ Map verticalStitch(Map m, int n, int l){
 			
 					m.pixels[i][ii]=m.pixels[i-1][ii];
 				}
+				Point p0 = m.pixels[removeSeam[ii]][ii];
+				Point p1 = m.pixels[removeSeam[ii]+1][ii];
 				m.pixels[removeSeam[ii]+1][ii].county=m.pixels[removeSeam[ii]][ii].county;
 				int v = m.pixels[removeSeam[ii]][ii].val;
 				int r = m.pixels[removeSeam[ii]][ii].data["r"];
@@ -2193,6 +2184,10 @@ void initialRun(){
 	}
 
 	
+	datas[0] = "d";
+	datas[1] = "r";
+	datas[2] = "d16";
+	datas[3] = "r16";
 	
 	
 	/*for (i=0;i<np;i++){
