7e7d77ef716d39719d9ae351aa09e558313e65fa
triplelog <robmwilc@gmail.com>
2020-10-01 12:02:57 -0400

    nm

--- seam-carving/scPrecinctsNew.py
+++ seam-carving/scPrecinctsNew.py
@@ -21,14 +21,15 @@ def readcsv(filen):
         return allgamesa
         
         
+        
+        
 shape = fiona.open("sc_2018/sc_2018.shp")
-
-shape16 = fiona.open("sc_2016/sc_2016.shp")
-
 print(len(shape))
-print(len(shape16))
+
 
 counties = [19,13,15,29,35]
+extradatas = ["d16","r16"]
+
 
 precincts = {}
 
@@ -61,6 +62,53 @@ for i in range(0,len(shape)):
 	if cent.y > bounds['top']:
 		bounds['top'] = cent.y
 print(bounds)
+
+for i in range(0,len(shape)):
+
+	if shape[i]['geometry']['type'] == "Polygon":
+		coords = shape[i]['geometry']['coordinates'][0]
+	else:
+		maxPoly = 0
+		for ii in range(0,len(shape[i]['geometry']['coordinates'])):
+			l = len(shape[i]['geometry']['coordinates'][ii][0])
+			if l>maxPoly:
+				maxPoly = l
+				coords = shape[i]['geometry']['coordinates'][ii][0]
+	try:
+		poly = Polygon(coords)
+	except:
+		#print(coords)
+		print(shape[i]['geometry']['type'])
+	cent = poly.centroid
+	if int(shape[i]['properties']['COUNTYFP']) not in counties:
+		continue
+	precincts[shape[i]['id']]={'x':(cent.x-bounds['left'])/(bounds['right']-bounds['left']),'y':(bounds['top']-cent.y)/(bounds['top']-bounds['bottom']),'name':shape[i]['properties']['NAME'],'d':shape[i]['properties']['G18GOVDSMI'],'r':shape[i]['properties']['G18GOVRMCM'],'county':shape[i]['properties']['COUNTYFP']}
+
+pixelMap = {}
+for precinct in precincts:
+
+	roundedX = round(precincts[precinct]['x']*100)
+	roundedY = round(precincts[precinct]['y']*100)
+	try:
+		pixelMap[roundedX*1000+roundedY]['d']+=int(precincts[precinct]['d'])
+		pixelMap[roundedX*1000+roundedY]['r']+=int(precincts[precinct]['r'])
+	except:
+		pixelMap[roundedX*1000+roundedY]={}
+		pixelMap[roundedX*1000+roundedY]['d']=int(precincts[precinct]['d'])
+		pixelMap[roundedX*1000+roundedY]['r']=int(precincts[precinct]['r'])
+		pixelMap[roundedX*1000+roundedY]['x']=round(precincts[precinct]['x']*100)
+		pixelMap[roundedX*1000+roundedY]['y']=round(precincts[precinct]['y']*100)
+		pixelMap[roundedX*1000+roundedY]['county']=int(precincts[precinct]['county'])
+		for i in range(0,len(extradatas)):
+			pixelMap[roundedX*1000+roundedY][extradatas[i]]=0
+
+
+
+
+
+
+shape16 = fiona.open("sc_2016/sc_2016.shp")
+print(len(shape16))
 bounds16 = {'left':1000000000,'right':-1000000000,'bottom':1000000000,'top':-1000000000}
 for i in range(0,len(shape16)):
 
@@ -91,31 +139,6 @@ for i in range(0,len(shape16)):
 		bounds16['top'] = cent.y
 print(bounds16)
 
-
-for i in range(0,len(shape)):
-
-	if shape[i]['geometry']['type'] == "Polygon":
-		coords = shape[i]['geometry']['coordinates'][0]
-	else:
-		maxPoly = 0
-		for ii in range(0,len(shape[i]['geometry']['coordinates'])):
-			l = len(shape[i]['geometry']['coordinates'][ii][0])
-			if l>maxPoly:
-				maxPoly = l
-				coords = shape[i]['geometry']['coordinates'][ii][0]
-	try:
-		poly = Polygon(coords)
-	except:
-		#print(coords)
-		print(shape[i]['geometry']['type'])
-	cent = poly.centroid
-	if int(shape[i]['properties']['COUNTYFP']) not in counties:
-		continue
-	precincts[shape[i]['id']]={'x':(cent.x-bounds['left'])/(bounds['right']-bounds['left']),'y':(bounds['top']-cent.y)/(bounds['top']-bounds['bottom']),'name':shape[i]['properties']['NAME'],'d':shape[i]['properties']['G18GOVDSMI'],'r':shape[i]['properties']['G18GOVRMCM'],'county':shape[i]['properties']['COUNTYFP']}
-
-
-
-
 precincts16 = {}
 
 for i in range(0,len(shape16)):
@@ -143,27 +166,9 @@ for i in range(0,len(shape16)):
 
 
 
-file1 = open('scpre.txt', 'w')
-file1.writelines(['std::map<int,Point> scPoints(){\n'])
-file1.writelines(['std::map<int,Point> points; int i=0;\n'])
 
-pixelMap = {}
-for precinct in precincts:
 
-	roundedX = round(precincts[precinct]['x']*100)
-	roundedY = round(precincts[precinct]['y']*100)
-	try:
-		pixelMap[roundedX*1000+roundedY]['d']+=int(precincts[precinct]['d'])
-		pixelMap[roundedX*1000+roundedY]['r']+=int(precincts[precinct]['r'])
-	except:
-		pixelMap[roundedX*1000+roundedY]={}
-		pixelMap[roundedX*1000+roundedY]['d']=int(precincts[precinct]['d'])
-		pixelMap[roundedX*1000+roundedY]['r']=int(precincts[precinct]['r'])
-		pixelMap[roundedX*1000+roundedY]['x']=round(precincts[precinct]['x']*100)
-		pixelMap[roundedX*1000+roundedY]['y']=round(precincts[precinct]['y']*100)
-		pixelMap[roundedX*1000+roundedY]['county']=int(precincts[precinct]['county'])
-		pixelMap[roundedX*1000+roundedY]['d16']=0
-		pixelMap[roundedX*1000+roundedY]['r16']=0
+
 
 		
 missingP = 0
@@ -213,12 +218,19 @@ for precinct in precincts16:
 				pixelMap[rXY]['r16']=int(precincts16[precinct]['r16'])
 					
 print(missingP)
+
+file1 = open('scpre.txt', 'w')
+file1.writelines(['std::map<int,Point> scPoints(){\n'])
+file1.writelines(['std::map<int,Point> points; int i=0;\n'])
 pixels = 0
 for pixel in pixelMap.keys():
 	pixels+=1
 	file1.writelines(['if (1==1){ Point p;\n'])
 	#line = str(precincts[precinct]['name'])+","+str(precincts[precinct]['x'])+","+str(precincts[precinct]['y'])+","+str(precincts[precinct]['d'])+","+str(precincts[precinct]['r'])+","+str(precincts[precinct]['county'])+"\n"
-	line = "p.x = "+str(pixelMap[pixel]['x'])+"; p.y = "+str(pixelMap[pixel]['y'])+"; p.county = "+str(pixelMap[pixel]['county'])+"; p.val = "+str(pixelMap[pixel]['d']+pixelMap[pixel]['r'])+"; p.data[\"d\"] = "+str(pixelMap[pixel]['d'])+"; p.data[\"r\"] = "+str(pixelMap[pixel]['r'])+"; p.data[\"d16\"] = "+str(pixelMap[pixel]['d16'])+"; p.data[\"r16\"] = "+str(pixelMap[pixel]['r16'])+";\n"
+	line = "p.x = "+str(pixelMap[pixel]['x'])+"; p.y = "+str(pixelMap[pixel]['y'])+"; p.county = "+str(pixelMap[pixel]['county'])+"; p.val = "+str(pixelMap[pixel]['d']+pixelMap[pixel]['r'])+"; p.data[\"d\"] = "+str(pixelMap[pixel]['d'])+"; p.data[\"r\"] = "+str(pixelMap[pixel]['r'])
+	for i in range(0,len(extradatas)):
+		line +="; p.data[\""+extradatas[i]+"\"] = "+str(pixelMap[pixel][extradatas[i]])
+	line +=";\n"
 	
 	file1.writelines([line])
 	file1.writelines(['points[i]=p;i++;}\n'])
