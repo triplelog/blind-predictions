afde184c7a5f9a1239a4ff5396a3a114f5faaef5
triplelog <robmwilc@gmail.com>
2020-10-01 16:34:52 -0400

    nm

--- seam-carving/scPrecinctsNew.py
+++ seam-carving/scPrecinctsNew.py
@@ -34,96 +34,22 @@ def readblockgroup(filen,state):
 		return allgamesa	   
 		
         
-shape = fiona.open("sc_2018/sc_2018.shp")
-print(len(shape))
+
 
 
 counties = [19,13,15,29,35]
-#extradatas = {"sc_2016/sc_2016.shp":{"d16":'G16PREDCLI',"r16":'G16PRERTRU'}}
+maindatas = {"val":'Total',"black":'Black or African American Alone',"white":'White Alone'}
 
-extradatas = {"sc_2016/sc_2016.shp":{"d16":'G16PREDCLI',"r16":'G16PRERTRU'},"demographics/tl_2019_45_bg/tl_2019_45_bg.shp":{"black":'Black or African American Alone',"white":'White Alone'}}
+extradatas = {"sc_2018/sc_2018.shp":{"r":'G18GOVDSMI',"d":'G18GOVRMCM'},"sc_2016/sc_2016.shp":{"d16":'G16PREDCLI',"r16":'G16PRERTRU'}}
 
 bgdata = readblockgroup("demographics/CVAP_2013-2017_ACS_csv_files/BlockGr.csv","45")
 
 
 
-precincts = {}
-
-bounds = {'left':1000000000,'right':-1000000000,'bottom':1000000000,'top':-1000000000}
-for i in range(0,len(shape)):
-
-	if shape[i]['geometry']['type'] == "Polygon":
-		coords = shape[i]['geometry']['coordinates'][0]
-	else:
-		maxPoly = 0
-		for ii in range(0,len(shape[i]['geometry']['coordinates'])):
-			l = len(shape[i]['geometry']['coordinates'][ii][0])
-			if l>maxPoly:
-				maxPoly = l
-				coords = shape[i]['geometry']['coordinates'][ii][0]
-	try:
-		poly = Polygon(coords)
-	except:
-		#print(coords)
-		print(shape[i]['geometry']['type'])
-	cent = poly.centroid
-	if int(shape[i]['properties']['COUNTYFP']) not in counties:
-		continue
-	if cent.x < bounds['left']:
-		bounds['left'] = cent.x
-	if cent.x > bounds['right']:
-		bounds['right'] = cent.x
-	if cent.y < bounds['bottom']:
-		bounds['bottom'] = cent.y
-	if cent.y > bounds['top']:
-		bounds['top'] = cent.y
-print(bounds)
-
-for i in range(0,len(shape)):
-
-	if shape[i]['geometry']['type'] == "Polygon":
-		coords = shape[i]['geometry']['coordinates'][0]
-	else:
-		maxPoly = 0
-		for ii in range(0,len(shape[i]['geometry']['coordinates'])):
-			l = len(shape[i]['geometry']['coordinates'][ii][0])
-			if l>maxPoly:
-				maxPoly = l
-				coords = shape[i]['geometry']['coordinates'][ii][0]
-	try:
-		poly = Polygon(coords)
-	except:
-		#print(coords)
-		print(shape[i]['geometry']['type'])
-	cent = poly.centroid
-	if int(shape[i]['properties']['COUNTYFP']) not in counties:
-		continue
-	precincts[shape[i]['id']]={'x':(cent.x-bounds['left'])/(bounds['right']-bounds['left']),'y':(bounds['top']-cent.y)/(bounds['top']-bounds['bottom']),'name':shape[i]['properties']['NAME'],'d':shape[i]['properties']['G18GOVDSMI'],'r':shape[i]['properties']['G18GOVRMCM'],'county':shape[i]['properties']['COUNTYFP']}
 
 pixelMap = {}
-for precinct in precincts:
 
-	roundedX = round(precincts[precinct]['x']*100)
-	roundedY = round(precincts[precinct]['y']*100)
-	try:
-		pixelMap[roundedX*1000+roundedY]['d']+=int(precincts[precinct]['d'])
-		pixelMap[roundedX*1000+roundedY]['r']+=int(precincts[precinct]['r'])
-	except:
-		pixelMap[roundedX*1000+roundedY]={}
-		pixelMap[roundedX*1000+roundedY]['d']=int(precincts[precinct]['d'])
-		pixelMap[roundedX*1000+roundedY]['r']=int(precincts[precinct]['r'])
-		pixelMap[roundedX*1000+roundedY]['x']=round(precincts[precinct]['x']*100)
-		pixelMap[roundedX*1000+roundedY]['y']=round(precincts[precinct]['y']*100)
-		pixelMap[roundedX*1000+roundedY]['county']=int(precincts[precinct]['county'])
-		for i in extradatas.keys():
-			for ii in extradatas[i].keys():
-				pixelMap[roundedX*1000+roundedY][ii]=0
-
-
-
-
-
-def addExtra(extrafile,idx):
+def addMain(extrafile):
 	shape16 = fiona.open(extrafile)
 	print(len(shape16))
 	precincts16 = {}
@@ -146,9 +72,8 @@ def addExtra(extrafile,idx):
 			print(shape16[i]['geometry']['type'])
 		cent = poly.centroid
 
-		if idx == 0 and int(shape16[i]['properties']['COUNTY']) not in counties:
-			continue
-		if idx == 1 and int(shape16[i]['properties']['COUNTYFP']) not in counties:
+
+		if int(shape16[i]['properties']['COUNTYFP']) not in counties:
 			continue
 		if cent.x < bounds16['left']:
 			bounds16['left'] = cent.x
@@ -180,16 +105,102 @@ def addExtra(extrafile,idx):
 			print(shape16[i]['geometry']['type'])
 		cent = poly.centroid
 		
-		if idx == 0 and int(shape16[i]['properties']['COUNTY']) not in counties:
+
+		if int(shape16[i]['properties']['COUNTYFP']) not in counties:
 			continue
-		if idx == 1 and int(shape16[i]['properties']['COUNTYFP']) not in counties:
+		precincts16[shape16[i]['id']]={'x':(cent.x-bounds16['left'])/(bounds16['right']-bounds16['left']),'y':(bounds16['top']-cent.y)/(bounds16['top']-bounds16['bottom'])}
+		for ii in maindatas.keys():
+			precincts16[shape16[i]['id']][ii]=bgdata[shape16[i]['properties']['GEOID']][maindatas[ii]]
+			
+
+		
+	missingP = 0
+	for precinct in precincts16:
+
+		roundedX = round(precincts16[precinct]['x']*100)
+		roundedY = round(precincts16[precinct]['y']*100)
+		try:
+			for ii in maindatas.keys():
+				pixelMap[roundedX*1000+roundedY][ii]+=int(precincts16[precinct][ii])
+		except:
+			pixelMap[roundedX*1000+roundedY]={}
+			for ii in maindatas.keys():
+				pixelMap[roundedX*1000+roundedY][ii]=int(precincts16[precinct][ii])
+			pixelMap[roundedX*1000+roundedY]['x']=round(precincts16[precinct]['x']*100)
+			pixelMap[roundedX*1000+roundedY]['y']=round(precincts16[precinct]['y']*100)
+			pixelMap[roundedX*1000+roundedY]['county']=0
+			for i in extradatas.keys():
+				for ii in extradatas[i].keys():
+					pixelMap[roundedX*1000+roundedY][ii]=0
+					
+addMain("demographics/tl_2019_45_bg/tl_2019_45_bg.shp")
+
+
+
+def addExtra(extrafile,idx):
+	shape16 = fiona.open(extrafile)
+	print(len(shape16))
+	precincts16 = {}
+	bounds16 = {'left':1000000000,'right':-1000000000,'bottom':1000000000,'top':-1000000000}
+	for i in range(0,len(shape16)):
+
+		if shape16[i]['geometry']['type'] == "Polygon":
+			coords = shape16[i]['geometry']['coordinates'][0]
+		else:
+			maxPoly = 0
+			for ii in range(0,len(shape16[i]['geometry']['coordinates'])):
+				l = len(shape16[i]['geometry']['coordinates'][ii][0])
+				if l>maxPoly:
+					maxPoly = l
+					coords = shape16[i]['geometry']['coordinates'][ii][0]
+		try:
+			poly = Polygon(coords)
+		except:
+			#print(coords)
+			print(shape16[i]['geometry']['type'])
+		cent = poly.centroid
+
+		if idx == 0 and int(shape16[i]['properties']['COUNTYFP']) not in counties:
+			continue
+		if idx == 1 and int(shape16[i]['properties']['COUNTY']) not in counties:
+			continue
+		if cent.x < bounds16['left']:
+			bounds16['left'] = cent.x
+		if cent.x > bounds16['right']:
+			bounds16['right'] = cent.x
+		if cent.y < bounds16['bottom']:
+			bounds16['bottom'] = cent.y
+		if cent.y > bounds16['top']:
+			bounds16['top'] = cent.y
+	print(bounds16)
+
+
+
+	for i in range(0,len(shape16)):
+
+		if shape16[i]['geometry']['type'] == "Polygon":
+			coords = shape16[i]['geometry']['coordinates'][0]
+		else:
+			maxPoly = 0
+			for ii in range(0,len(shape16[i]['geometry']['coordinates'])):
+				l = len(shape16[i]['geometry']['coordinates'][ii][0])
+				if l>maxPoly:
+					maxPoly = l
+					coords = shape16[i]['geometry']['coordinates'][ii][0]
+		try:
+			poly = Polygon(coords)
+		except:
+			#print(coords)
+			print(shape16[i]['geometry']['type'])
+		cent = poly.centroid
+		
+		if idx == 0 and int(shape16[i]['properties']['COUNTYFP']) not in counties:
+			continue
+		if idx == 1 and int(shape16[i]['properties']['COUNTY']) not in counties:
 			continue
 		precincts16[shape16[i]['id']]={'x':(cent.x-bounds16['left'])/(bounds16['right']-bounds16['left']),'y':(bounds16['top']-cent.y)/(bounds16['top']-bounds16['bottom'])}
 		for ii in extradatas[extrafile].keys():
-			if idx == 0:
-				precincts16[shape16[i]['id']][ii]=shape16[i]['properties'][extradatas[extrafile][ii]]
-			elif idx == 1:
-				precincts16[shape16[i]['id']][ii]=bgdata[shape16[i]['properties']['GEOID']][extradatas[extrafile][ii]]
+			precincts16[shape16[i]['id']][ii]=shape16[i]['properties'][extradatas[extrafile][ii]]
 			
 
 		
@@ -230,6 +241,8 @@ def addExtra(extrafile,idx):
 					pixelMap[rXY]['r']=0
 					pixelMap[rXY]['x']=round(precincts16[precinct]['x']*100)
 					pixelMap[rXY]['y']=round(precincts16[precinct]['y']*100)
+					for ii in maindatas.keys():
+						pixelMap[rXY][ii]=0
 					for i in extradatas.keys():
 						for ii in extradatas[i].keys():
 							pixelMap[rXY][ii]=0
@@ -259,7 +272,11 @@ for pixel in pixelMap.keys():
 	pixels+=1
 	file1.writelines(['if (1==1){ Point p;\n'])
 	#line = str(precincts[precinct]['name'])+","+str(precincts[precinct]['x'])+","+str(precincts[precinct]['y'])+","+str(precincts[precinct]['d'])+","+str(precincts[precinct]['r'])+","+str(precincts[precinct]['county'])+"\n"
-	line = "p.x = "+str(pixelMap[pixel]['x'])+"; p.y = "+str(pixelMap[pixel]['y'])+"; p.county = "+str(pixelMap[pixel]['county'])+"; p.val = "+str(pixelMap[pixel]['d']+pixelMap[pixel]['r'])+"; p.data[\"d\"] = "+str(pixelMap[pixel]['d'])+"; p.data[\"r\"] = "+str(pixelMap[pixel]['r'])
+	line = "p.x = "+str(pixelMap[pixel]['x'])+"; p.y = "+str(pixelMap[pixel]['y'])+"; p.county = "+str(pixelMap[pixel]['county'])+"; p.val = "+str(pixelMap[pixel]['val'])
+	for ii in maindatas.keys():
+		if ii == "val":
+			continue
+		line +="; p.data[\""+ii+"\"] = "+str(pixelMap[pixel][ii])
 	for i in extradatas.keys():
 		for ii in extradatas[i].keys():
 			line +="; p.data[\""+ii+"\"] = "+str(pixelMap[pixel][ii])
