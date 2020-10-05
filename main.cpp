627b22ef0c94ef9b663b17ebafb2f25256a48b19
triplelog <robmwilc@gmail.com>
2020-10-05 10:28:20 -0400

    nm

--- main.cpp
+++ main.cpp
@@ -14,11 +14,9 @@
 
 #include "data.cpp"
 
-extern "C" {
-# include "lua-5.4.0/src/lua.h"
-# include "lua-5.4.0/src/lauxlib.h"
-# include "lua-5.4.0/src/lualib.h"
-}
+
+#include "lua-5.4.0/src/lua.hpp"
+
 #include <emscripten/emscripten.h>
 
 EM_JS(void, console_log, (int x), {
