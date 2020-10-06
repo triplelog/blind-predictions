f24c1910c36e1cb510548abd4070450771fd21d9
triplelog <robmwilc@gmail.com>
2020-10-06 18:09:07 -0400

    nm

--- static/js/map.js
+++ static/js/map.js
@@ -454,7 +454,6 @@ myWorker.onmessage = function(e) {
 		orderStates();
 	}
 	else if (e.data['type'] == "wins"){
-		console.log(e.data);
 		var dw = parseInt(e.data.D);
 		var rw = parseInt(e.data.R);
 		var tw = parseInt(e.data.T);
