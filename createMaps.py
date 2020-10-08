c47f8b855f497d5c76f1cffb27723c23405df160
triplelog <robmwilc@gmail.com>
2020-10-08 09:33:16 -0400

    nm

--- createMaps.py
+++ createMaps.py
@@ -34,11 +34,19 @@ for i in range(0,len(lines)):
 		continue
 	startState = '<g fill="none" stroke="#000000" stroke-opacity="1" stroke-width="3.07087"'
 	if line[:len(startState)] == startState:
-		line = '<g id="state-'+states[idx]+'" fill="none" stroke="#000000" stroke-opacity="1" stroke-width="3.07087" stroke-linecap="square" stroke-linejoin="bevel" transform="matrix(1,0,0,1,105.651,45.8515)"'
+		if idx == 0:
+			line = '''<text id="state-me1" class="districtText" font-size="3" stroke="none" x="260.5" y="93">1</text>
+<text id="state-me2" class="districtText" font-size="3" stroke="none" x="263.5" y="93">2</text>
+<text id="state-ne1" class="districtText" font-size="3" stroke="none" x="189.5" y="102.5">1</text>
+<text id="state-ne2" class="districtText" font-size="3" stroke="none" x="192.5" y="102.5">2</text>
+<text id="state-ne3" class="districtText" font-size="3" stroke="none" x="195.5" y="102.5">3</text>\n'''
+		else:
+			line = ""
+		line += '<g id="state-'+states[idx].lower()+'" fill="none" stroke="#000000" stroke-opacity="1" stroke-width="3.07087" stroke-linecap="square" stroke-linejoin="bevel" transform="matrix(1,0,0,1,105.651,45.8515)"'
 		line += ' font-family="MS Shell Dlg 2" font-size="34.375" font-weight="400" font-style="normal">\n'
 		skip = 2
 		idx+=1
-	#lineSplit = line.strip().split(",")
-
 	file1.writelines([line])
+	
+	
 file1.writelines(end)
