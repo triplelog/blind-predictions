34ceba0215032cdd84e7649f76044e78e2af7c44
triplelog <robmwilc@gmail.com>
2020-10-08 15:12:51 -0400

    nm

--- static/js/map.js
+++ static/js/map.js
@@ -466,7 +466,7 @@ myWorker.onmessage = function(e) {
 						absDiff = -1*absDiff;
 					}
 					//var delay = 5000*(−0.0013*Math.pow(absDiff,3)+0.077*Math.pow(absDiff,2)−1.61*absDiff+12);
-					var delay = 5000*(−0.0013*Math.pow(absDiff,3));
+					var delay = 5000*(-0.0013*Math.pow(absDiff,3));
 					if (delay <= 0){
 						electoralData[ii].rpred = (.5 + e.data['val'][i]/-2000)/demoMult;
 					}
