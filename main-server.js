c60bf3deae6e22a32aae2660fed6280ef320ab4a
triplelog <robmwilc@gmail.com>
2020-10-14 13:30:51 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -282,7 +282,6 @@ function loadAllData() {
 			}
 			var year = parseInt(data.data[i][0])-2000;
 			var state = csvdata['convert']['states'][data.data[i][2]];
-			console.log(state);
 			if (data.data[i][7] =="D"){
 				csvdata['states'][state]['538Preddwin'+year]=parseFloat(data.data[i][11]);
 				if (csvdata['states'][state]['538Preddelo'+year]){
