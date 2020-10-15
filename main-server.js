614abec17f0a37ae6adcfe0e1dcb7aa7f9e04ec3
triplelog <robmwilc@gmail.com>
2020-10-15 10:25:19 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -105,8 +105,8 @@ wss.on('connection', function connection(ws) {
 						row['id']=state;
 					}
 					else {
-						console.log(colArray[i].round);
 						row[colArray[i].field]=Math.round(csvdata['states'][state][colArray[i].field],colArray[i].round);
+						console.log(csvdata['states'][state][colArray[i].field]);
 					}
 				}
 				tableData.push(row);
