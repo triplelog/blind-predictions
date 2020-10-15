e8b1f4e3801f64091cff66c5dbd99121bdd4708d
triplelog <robmwilc@gmail.com>
2020-10-15 19:22:07 -0400

    nm

--- main-server.js
+++ main-server.js
@@ -98,7 +98,7 @@ wss.on('connection', function connection(ws) {
 				}
 			}
 			else if (dm.computecolumn){
-				var newColumn = [dm.computecolumn,1,false,dm.name];
+				var newColumn = [postfixify(dm.computecolumn),1,false,dm.name];
 				newColumn.push(dm.name);
 				myColumns.push(newColumn);
 				colArray = [{'title':'ID','field':'id'}];
@@ -124,6 +124,8 @@ wss.on('connection', function connection(ws) {
 			
 	
 			var tableData = [];
+			var letters = ["A","B","C",'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+			
 			for (var state in csvdata['states']){
 				if (locations == 50 && (state == "DC" || state.substr(1) == "0" || state.substr(1) == "1" || state.substr(1) == "2" || state.substr(1) == "3") ){
 					continue;
@@ -135,25 +137,29 @@ wss.on('connection', function connection(ws) {
 					continue;
 				}
 				var row = {};
+				var valMap = {};
 				for (var i=0;i<colArray.length;i++){
 					if (colArray[i].field == "id"){
 						row['id']=state;
 					}
 					else {
+						
 						if (colArray[i].round<0){
 							row[colArray[i].field]=csvdata['states'][state][colArray[i].field];
+							
 						}
 						else if (colArray[i].formula){
 							
 						}
 						else {
 							row[colArray[i].field]=Math.round(csvdata['states'][state][colArray[i].field]*Math.pow(10,colArray[i].round))/Math.pow(10,colArray[i].round);
+							valMap[letters[i-1]]=csvdata['states'][state][colArray[i].field];
 						}
 					}
 				}
 				for (var i=0;i<colArray.length;i++){
 					if (colArray[i].formula){
-						row[colArray[i].field]=colArray[i].formula;
+						row[colArray[i].field]=valMap[colArray[i].formula];
 
 					}
 				}
@@ -1163,3 +1169,162 @@ app.get(['*'],
 
 
 
+function makePost(infixexpr) {
+	prec = {}
+	prec["*"] = 4
+	prec["/"] = 4
+	prec["+"] = 3
+	prec["~"] = 3
+	prec[">"] = 2
+	prec["<"] = 2
+	prec["="] = 2
+	prec["!"] = 2
+	prec["["] = 2
+	prec["]"] = 2
+	prec["&"] = 1
+	prec["|"] = 0
+	prec["("] = -1
+	opStack = []
+	postfixList = []
+	intstr = ''
+	expstr = ''
+	tokenList = []
+	temptoken = ''
+	for (var i=0;i<infixexpr.length;i++){
+		var ie = infixexpr[i];
+		if ("-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(ie) > -1){
+			temptoken += ie
+		}
+		else{
+			if (temptoken != ''){
+				tokenList.push(temptoken)
+			}
+			tokenList.push(ie)
+			temptoken = ''
+		}
+	}
+	if (temptoken != ''){
+		tokenList.push(temptoken)
+	}
+	
+	for (var i=0;i<tokenList.length;i++){
+		var token = tokenList[i];
+		if ("*/+~><=![]&|()".indexOf(token) == -1){
+			postfixList.push(token)
+		}
+		else if (token == '('){
+			opStack.push(token)
+		}
+		else if (token == ')'){
+			topToken = opStack.pop()
+			while (topToken != '('){
+				postfixList.push(topToken)
+				topToken = opStack.pop()
+			}
+		}
+		else {
+			while ((opStack.length > 0) && (prec[opStack[opStack.length-1]] >= prec[token])){
+				postfixList.push(opStack.pop())
+			}
+			opStack.push(token)
+		}
+	}
+	while (opStack.length > 0){
+		postfixList.push(opStack.pop())
+	}
+	for (var i=0;i<postfixList.length;i++){
+		var ci = postfixList[i];
+		if ("*/+~><=![]&|".indexOf(ci) == -1){
+			intstr += ci + '_'
+			expstr += '#'
+		}
+		else if (ci == '~'){
+			expstr += '-'
+		}
+		else{
+			expstr += ci
+		}
+	}
+	intstr = intstr.substring(0,intstr.length-1)
+	return [intstr,expstr]
+
+}
+
+function replaceDecimals(istr){
+	dindex = istr.indexOf('.');
+	while (dindex >-1){
+		intpart = 0;
+		decpart = 0;
+		denom = 1;
+		strparts = [dindex,dindex+1];
+		for (var i=1;i<dindex+1;i++){
+			if ("0123456789".indexOf(istr[dindex-i]) > -1){
+				intpart += parseInt(istr[dindex-i])*Math.pow(10,i-1);
+				strparts[0] = dindex-i;
+			}
+			else{break;}
+		}
+		for (var i=dindex+1;i<istr.length;i++){
+			if ("0123456789".indexOf(istr[i]) > -1){
+				decpart *=10;
+				denom *=10;
+				decpart += parseInt(istr[i]);
+				strparts[1] = i+1;
+			}
+			else{break;}
+		}
+		istr = istr.substring(0,strparts[0])+'('+ (intpart*denom+decpart) +'/'+ denom +')'+istr.substring(strparts[1],);
+		dindex = istr.indexOf('.');
+	}
+
+	return istr
+}
+
+function replaceNegatives(istr){
+	dindex = istr.indexOf('-')
+	while (dindex >-1){
+		if (dindex == 0){
+			if ("0123456789".indexOf(istr[1]) == -1) {
+				istr = '-1*'+istr.substring(1,);
+			}
+			dindex = istr.indexOf('-',1);
+		}
+		else{
+			if ("><=![]&|(".indexOf(istr[dindex-1])> -1) {
+				if ("0123456789".indexOf(istr[dindex-1])== -1){
+					istr = istr.substring(0,dindex)+'-1*'+istr.substring(dindex+1,);
+				}
+				dindex = istr.indexOf('-',dindex+1);
+			}
+			else{
+				istr = istr.substring(0,dindex)+'~'+istr.substring(dindex+1,);
+				dindex = istr.indexOf('-',dindex+1);
+			}
+		}
+	}
+				
+	return istr
+}
+
+function postfixify(input_str) {
+	input_str = input_str.toUpperCase();
+	input_str = input_str.replace(/AND/g,'&');
+	input_str = input_str.replace(/OR/g,'|');
+	input_str = input_str.replace(/\[/g,'(');
+	input_str = input_str.replace(/]/g,')');
+	input_str = input_str.replace(/{/g,'(');
+	input_str = input_str.replace(/}/g,')');
+	input_str = input_str.replace(/>=/g,']');
+	input_str = input_str.replace(/<=/g,'[');
+	input_str = input_str.replace(/==/g,'=');
+	input_str = input_str.replace(/!=/g,'!');
+	input_str = input_str.replace(/\+-/g,'-');
+	input_str = input_str.replace(/--/g,'+');
+	input_str = replaceDecimals(input_str);
+	input_str = replaceNegatives(input_str);
+	var twoparts = makePost(input_str);
+	//Convert column names
+	console.log(twoparts[0]);
+	console.log(twoparts[1]);
+
+}
