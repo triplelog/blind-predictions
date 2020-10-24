9e6603889da31323eead9a162439f2664b5dab30
triplelog <robmwilc@gmail.com>
2020-10-24 09:20:12 -0400

    nm

---
+++ staterandomforest.py
@@ -0,0 +1,40 @@
+import time
+import sys
+import random
+import csv
+import math
+import json
+import numpy
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+
+def pywin(dperc):
+	return 1.0/(1.0+((1.01-dperc)/(dperc+.01))**7.5)
+def undopywin(x,tvotes):
+	ndistricts = tvotes*1.0/200000
+	exp = 2.0+4.0/ndistricts
+	a = (1.0/x-1.0)**(1.0/exp)
+	return (1.01-.01*a)/(a+1)
+	
+def writecsv(parr, filen):
+        with open(filen, 'w') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                for i in range(0,len(parr)):
+                        try:
+                                spamwriter.writerow(parr[i])
+                        except:
+                                print(parr[i], i)
+
+
+
+def readcsv(filen):
+        allgamesa  =[]
+        with open(filen, 'r') as csvfile:
+                spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+                for row in spamreader:
+                        allgamesa.append(row)
+        return allgamesa
+        
+allPres = readcsv("data/1976-2016-president.csv")
+print(allPres)
