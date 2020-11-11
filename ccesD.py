851a0d1c6ae1909d38b772b869da48b2dc851d47
triplelog <robmwilc@gmail.com>
2020-11-11 10:38:40 -0500

    nm

--- ccesD.py
+++ ccesD.py
@@ -139,11 +139,11 @@ for i in range(1,8):
 	s = [0,0,0]
 	for ii in range(1,8):
 		n[0]+=ratings['T'][i][ii]
-		s[0]+=ratings['T'][i][ii]*i
+		s[0]+=ratings['T'][i][ii]*ii
 		n[1]+=ratings['D'][i][ii]
-		s[1]+=ratings['D'][i][ii]*i
+		s[1]+=ratings['D'][i][ii]*ii
 		n[2]+=ratings['R'][i][ii]
-		s[2]+=ratings['R'][i][ii]*i
+		s[2]+=ratings['R'][i][ii]*ii
 	print('T ',ratings['T'][i],s[0]/n[0])
 	print('D ',ratings['D'][i],s[1]/n[1])
 	print('R ',ratings['R'][i],s[2]/n[2])
